b015feeb3f5f023211f83a111d7a2c42d86f32b7
justwoork <157926518+justwoork@users.noreply.github.com>
2025-01-10 16:27:04 +0300

    add model to admin page

--- payments/admin.py
+++ payments/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Phone, Transaction
 
-# Register your models here.
+@admin.register(Phone)
+class PhoneAdmin(admin.ModelAdmin):
+    list_display = ('phone_number', 'provider', 'amount', 'payment_day', 'payment_time')
+    search_fields = ('phone_number', 'provider')
+    list_filter = ('provider',)
+
+@admin.register(Transaction)
+class TransactionAdmin(admin.ModelAdmin):
+    list_display = ('phone', 'amount', 'status', 'created_at', 'yoomoney_transaction_id')
+    search_fields = ('phone__phone_number', 'yoomoney_transaction_id')
+    list_filter = ('status', 'created_at')
